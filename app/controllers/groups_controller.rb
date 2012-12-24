4728c7eceb46b55f72cf7175dd60c08561e948ba
luojianming <luojmfly@gmail.com>
2012-12-25 00:03:17 +0800

    fixed group page v2

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 class GroupsController < ApplicationController
   load_and_authorize_resource
   def index
@@ -18,7 +19,7 @@ class GroupsController < ApplicationController
 
 
     @group = current_user.mygroups.build(params[:group])
-    respond_to do |format|
+     respond_to do |format|
       if @group.save
         format.html { redirect_to root_path, notice: 'Group was successfully created.' }
         format.json { render json: @profile, status: :created, location: @profile }
@@ -33,15 +34,28 @@ class GroupsController < ApplicationController
   end
 
   def update
+    @group = Group.find(params[:id])
+    params[:group][:status] = @group.status
+    @num = @group.group_memberships.count
+    for i in  0..@num-1
+      params[:group][:group_memberships_attributes][i.to_s][:status] = @group.group_memberships[i].status
+    end
+    if @group.update_attributes(params[:group])
+      redirect_to group_path(@group), :notice => "更新成功"
+    else
+      redirect_to group_path(@group), :notice => "更新失败"
+    end
   end
 
   def new
     @group = current_user.mygroups.build
-    3.times {
+     3.times {
       @group.group_memberships.build
     }
   end
 
   def edit
+    @group = Group.find(params[:id])
+    render 'new'
   end
 end
